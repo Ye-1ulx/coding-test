976f6fbe5d46fc98c461ba1d4b22ce3b0f0d8d72
Ye-1ulx <s25014@gsm.hs.kr>
2025-10-13 14:36:02 +0900

    나이 출력

--- java/나이 출력/나이 출력.java
+++ java/나이 출력/나이 출력.java
@@ -1,7 +1,6 @@
-#include <stdio.h>
-#include <stdbool.h>
-#include <stdlib.h>
-int solution(int age) {
-    int answer = 2022-age+1;
-    return answer;
+class Solution {
+    public int solution(int age) {
+        int answer = 2022-age+1;
+        return answer;
+    }
 }
